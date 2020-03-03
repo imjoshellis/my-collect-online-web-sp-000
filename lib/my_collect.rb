9414b31456a17457596fbd8fef5c8797b7254fd0
Josh Ellis <joshe523@gmail.com>
2020-03-03 03:26:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+#
+# => ["Tim", "Tom", "Jim"]
+#
 
-
+def my_collect(arr)
+  i = 0
+  return_arr = []
+  while i < arr.size do
+    return_arr << yield(arr[i])
+  end
+  return return_arr
+end
